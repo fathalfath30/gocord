894c0b484b30983960fe944bd6abaa6ba1f28073
fathalfath30 <fathalfath30@gmail.com>
2023-06-08 22:15:01 +0700

    feat: adding new variable and constant

---
+++ gocord/var.go
@@ -0,0 +1,52 @@
+/*
+//
+//  ______    _   _           _  __      _   _     ____   ___
+// |  ____|  | | | |         | |/ _|    | | | |   |___ \ / _ \
+// | |__ __ _| |_| |__   __ _| | |_ __ _| |_| |__   __) | | | |
+// |  __/ _` | __| '_ \ / _` | |  _/ _` | __| '_ \ |__ <| | | |
+// | | | (_| | |_| | | | (_| | | || (_| | |_| | | |___) | |_| |
+// |_|  \__,_|\__|_| |_|\__,_|_|_| \__,_|\__|_| |_|____/ \___/
+//
+// Written by Fathalfath30.
+// Email : fathalfath30@gmail.com
+// Follow me on:
+//  Github : https://github.com/fathalfath30
+//  Gitlab : https://gitlab.com/Fathalfath30
+//
+*/
+
+package gocord
+
+var (
+	LocaleIndonesian    = Locale{Locale: "id", LanguageName: "Indonesian", NativeName: "Bahasa Indonesia"}
+	LocaleDanish        = Locale{Locale: "da", LanguageName: "Danish", NativeName: "Dansk"}
+	LocaleGerman        = Locale{Locale: "ge", LanguageName: "German", NativeName: "Deutsch"}
+	LocaleEnglishUK     = Locale{Locale: "en-GB", LanguageName: "English, UK", NativeName: "English, UK"}
+	LocaleEnglishUS     = Locale{Locale: "en-US", LanguageName: "English, US", NativeName: "English, US"}
+	LocaleSpanish       = Locale{Locale: "es-ES", LanguageName: "Spanish", NativeName: "Español"}
+	LocaleFrench        = Locale{Locale: "fr", LanguageName: "French", NativeName: "Français"}
+	LocaleCroatian      = Locale{Locale: "hr", LanguageName: "Croatian", NativeName: "Hrvatski"}
+	LocaleItalian       = Locale{Locale: "it", LanguageName: "Italian", NativeName: "Italiano"}
+	LocaleLithuanian    = Locale{Locale: "lt", LanguageName: "Lithuanian", NativeName: "Lietuviškai"}
+	LocaleHungarian     = Locale{Locale: "hu", LanguageName: "Hungarian", NativeName: "Magyar"}
+	LocaleDutch         = Locale{Locale: "nl", LanguageName: "Dutch", NativeName: "Nederlands"}
+	LocaleNorwegian     = Locale{Locale: "no", LanguageName: "Norwegian", NativeName: "Norsk"}
+	LocalePolish        = Locale{Locale: "pl", LanguageName: "Polish", NativeName: "Polski"}
+	LocaleBrazilian     = Locale{Locale: "pt-BR", LanguageName: "Portuguese, Brazilian", NativeName: "Português do Brasil"}
+	LocaleRomanian      = Locale{Locale: "ro", LanguageName: "Romanian, Romania", NativeName: "Română"}
+	LocaleFinnish       = Locale{Locale: "fi", LanguageName: "Finnish", NativeName: "Suomi"}
+	LocaleSwedish       = Locale{Locale: "sv-SE", LanguageName: "Swedish", NativeName: "Svenska"}
+	LocaleVietnamese    = Locale{Locale: "vi", LanguageName: "Vietnamese", NativeName: "Tiếng Việt"}
+	LocaleTurkish       = Locale{Locale: "tr", LanguageName: "Turkish", NativeName: "Türkçe"}
+	LocaleCzech         = Locale{Locale: "cs", LanguageName: "Czech", NativeName: "Čeština"}
+	LocaleGreek         = Locale{Locale: "el", LanguageName: "Greek", NativeName: "Ελληνικά"}
+	LocaleBulgarian     = Locale{Locale: "bg", LanguageName: "Bulgarian", NativeName: "български"}
+	LocaleRussian       = Locale{Locale: "ru", LanguageName: "Russian", NativeName: "Pусский"}
+	LocaleUkrainian     = Locale{Locale: "uk", LanguageName: "Ukrainian", NativeName: "Українська"}
+	LocaleHindi         = Locale{Locale: "hi", LanguageName: "Hindi", NativeName: "हिन्दी"}
+	LocaleThai          = Locale{Locale: "th", LanguageName: "Thai", NativeName: "ไทย"}
+	LocaleChinese       = Locale{Locale: "zh-CN", LanguageName: "Chinese, China", NativeName: "中文"}
+	LocaleJapanese      = Locale{Locale: "ja", LanguageName: "Japanese", NativeName: "日本語"}
+	LocaleChineseTaiwan = Locale{Locale: "zh-TW", LanguageName: "Chinese, Taiwan", NativeName: "繁體中文"}
+	LocaleKorean        = Locale{Locale: "ko", LanguageName: "Korean", NativeName: "한국어"}
+)
